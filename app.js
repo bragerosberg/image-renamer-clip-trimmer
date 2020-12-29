3eefb1416401f4c6ef66ba5ee974e059138ba483
Brage Rosberg <brage70@gmail.com>
2020-12-29 13:49:57 +0100

    2t/s

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const fs = require('fs'); 
 const glob = require("glob");
 
-// Global Variables
-const imageFormat = "jpg";
-const year = 1995;
+// Config Variables
+const imageFormat = "jpg"; 
+const baseName = 1995; // i.e. 1995 would return -> 1995_0.jpg, 1995_1.jpg, 1995_2.jpg ...
 
-glob(`media/*.${imageFormat}`, (er, images) => {
-  images.forEach((img, i) => fs.rename(img, `media/${year}_${i}.${imageFormat}`, () => {
-    console.log(`${img} renamed to ${year}_${i}.${imageFormat}`);
+glob(`media/*.${imageFormat}`, (err, images) => {
+  images.forEach((img, i) => fs.rename(img, `media/${baseName}_${i}.${imageFormat}`, () => {
+    console.log(`${img} renamed to ${baseName}_${i}.${imageFormat}`);
+    if(err) console.error(err);
   }));
 });
