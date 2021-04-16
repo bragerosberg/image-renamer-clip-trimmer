1c0b8ab87537af7fd672dea2af00fff5bdc57ff9
Brage Rosberg <brage70@gmail.com>
2021-04-16 19:11:46 +0200

    Update variable

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const fs = require('fs'); 
+const fs = require("fs");
 const glob = require("glob");
 
-// Config Variables
-const imageFormat = "jpg"; 
-const baseName = 1995; // 1995_0.jpg, 1995_1.jpg, 1995_2.jpg
+const imageFormat = "jpg";
+const fileName = 1995; // 1995_0.jpg, 1995_1.jpg, 1995_2.jpg
 
 glob(`media/*.${imageFormat}`, (err, images) => {
-  images.forEach((img, i) => fs.rename(img, `media/${baseName}_${i}.${imageFormat}`, () => {
-    console.log(`${img} renamed to ${baseName}_${i}.${imageFormat}`);
-    if(err) console.error(err);
-  }));
+  images.forEach((img, i) =>
+    fs.rename(img, `media/${fileName}_${i}.${imageFormat}`, () => {
+      console.log(`${img} renamed to ${fileName}_${i}.${imageFormat}`);
+      if (err) console.error(err);
+    })
+  );
 });
