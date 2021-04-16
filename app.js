64a31049de4a31e98aa71206f2b3bba59b64a914
Brage Rosberg <bragerosberg@Brages-MacBook-Pro.local>
2021-04-16 19:08:30 +0200

    Update comment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const glob = require("glob");
 
 // Config Variables
 const imageFormat = "jpg"; 
-const baseName = 1995; // 1995 return -> 1995_0.jpg, 1995_1.jpg, 1995_2.jpg
+const baseName = 1995; // 1995_0.jpg, 1995_1.jpg, 1995_2.jpg
 
 glob(`media/*.${imageFormat}`, (err, images) => {
   images.forEach((img, i) => fs.rename(img, `media/${baseName}_${i}.${imageFormat}`, () => {
