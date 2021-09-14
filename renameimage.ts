cb20be3767ce706b970df25ded17303c82d753b0
Brage Røsberg <brage70@gmail.com>
2021-09-14 15:29:03 +0200

    Update type errors, and minor corrections in renameimage.ts

--- renameimage.ts
+++ renameimage.ts
@@ -1,17 +1,21 @@
-const fs = require("fs");
-const glob = require("glob");
+const fs = require('fs');
+const glob = require('glob');
 
-const imageFormat = "jpg";
-const fileName = 2020;
+const imageFormat = 'jpg';
+const fileName = 2021;
+const prefix = 'Mai';
 
-let index = 0;
-
-glob(`media/*.${imageFormat}`, (err, images) => {
-  images.forEach((img, i) => {
-    index += 1;
-    fs.rename(img, `media/${index}_2020.${imageFormat}`, () => {
-      console.log(`${img} ${i} renamed to ${fileName}_${i}.${imageFormat}`);
-      if (err) console.error(err);
+export const renameImages = () => {
+  glob(`media/*.${imageFormat}`, (err, images) => {
+    images.forEach((img, i) => {
+      fs.rename(img, `media/${prefix}-${fileName}-${i}.${imageFormat}`, () => {
+        console.log(
+          `${img} ${i} renamed to ${prefix}-${fileName}-${i}.${imageFormat}`
+        );
+        if (err) console.error(err);
+      });
     });
   });
-});
+};
+
+renameImages();
--- trimmp4.ts
+++ trimmp4.ts
@@ -2,23 +2,29 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 const glob = require('glob');
 const fs = require('fs');
-ffmpeg.setFfmpegPath(ffmpegPath)
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 const clipFormat = 'mp4';
-glob(`*.${clipFormat}`, (_, clips) => {
-  clips.forEach((clip, i) => {
-   ffmpeg(clip)
-    .setStartTime('00:02:30')
-    .setDuration(30)
-    .output(`${clip}_${i}.${clipFormat}`)
-    .on('end', (err) => {
-      if(!err) { 
-        console.log(`${clip} converted`) 
-        fs.unlinkSync(clip);
-      }
-    })
-    .on('error', (err) => {
-      console.error(err)
-    }).run()
+
+export const trimClip = () => {
+  glob(`*.${clipFormat}`, (_, clips) => {
+    clips.forEach((clip, i) => {
+      ffmpeg(clip)
+        .setStartTime('00:02:30')
+        .setDuration(30)
+        .output(`${clip}_${i}.${clipFormat}`)
+        .on('end', err => {
+          if (!err) {
+            console.log(`${clip} converted`);
+            fs.unlinkSync(clip);
+          }
+        })
+        .on('error', err => {
+          console.error(err);
+        })
+        .run();
+    });
   });
-});
+};
+
+trimClip();
